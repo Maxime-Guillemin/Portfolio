79106e45bee4e971792248084e38236f5e9077cb
Maxime Guillemin <73181691+Maxime-Guillemin@users.noreply.github.com>
2023-10-16 16:42:14 +0200

    time line

--- src/components/Diplomes/Diplomes.js
+++ src/components/Diplomes/Diplomes.js
@@ -1,36 +1,101 @@
-import React from "react";
-import { Timeline } from "antd";
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+import {
+  TbBellSchool, TbSchool
+} from "react-icons/tb";
+import {
+  GiSchoolBag
+} from "react-icons/gi"
+import {
+  MdComputer
+}from "react-icons/md"
 
 import "./Diplomes.css";
 
 function Diplomes() {
   return (
     <div className="mainContainer">
-      <Timeline
-        mode="alternate"
-        items={[
-          {
-            label: " 2016 - 2019",
-            children: "Baccalauréat STI2D Laon",
-            color: "#1554ad",
-          },
-          {
-            label: "2019 - 2021",
-            children: "DUT Informatique Amiens ",
-            color: "#3c8618",
-          },
-          {
-            label: "2021-2022",
-            children: `Licence Pro RGI Amiens  `,
-            color: "#3c8618",
-          },
-          {
-            label: "2022-2024",
-            children: "Master MIAGE Amiens  ",
-            color: "#a02669",
-          },
-        ]}
-      />
+      <VerticalTimeline
+      lineColor={ '#d7a173' }>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "#2D4059", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "#2D4059" }}
+          date="2022 - 2024 à  Amiens"
+          iconStyle={{ background: "#2D4059", color: "#fff" }}
+          icon={<TbSchool />}
+        >
+          <h3 className="vertical-timeline-element-title">Master MIAGE</h3>
+          <h6 className="vertical-timeline-element-subtitle">
+            Master Méthodes Informatiques Appliquées à la Gestion des
+            Entreprises
+          </h6>
+          <p>
+            Formation en informatique, avec spécialisation en développement web
+            et gestion de projet, acquise en 2 ans en alternance.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "#E84545", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  #E84545" }}
+          date="2021 - 2022 à Amiens"
+          iconStyle={{ background: "#E84545", color: "#fff" }}
+          icon={<MdComputer />}
+        >
+          <h3 className="vertical-timeline-element-title">Licence Pro RGI</h3>
+          <h6 className="vertical-timeline-element-subtitle">
+            Licence professionnelle Réseaux et Génie Informatique
+          </h6>
+          <p>
+            Formation en informatique, avec spécialisation en développement web
+            et framework. Réalisé en un an en alternance.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "#E84545", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  #E84545" }}
+          date="2019 - 2021 à Amiens"
+          iconStyle={{ background: "#E84545", color: "#fff" }}
+          icon={<MdComputer />}
+        >
+          <h3 className="vertical-timeline-element-title">DUT Informatique</h3>
+          <h6 className="vertical-timeline-element-subtitle">
+            Diplôme Universitaire de Technologie Informatique
+          </h6>
+          <p>Acquisition des bases de l'informatique. Réalisé en deux ans</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="2017-2019 à Laon"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<TbBellSchool />}
+        >
+          <h3 className="vertical-timeline-element-title">Bac STI2D</h3>
+          <h6 className="vertical-timeline-element-subtitle">Baccalauréat Science et Technologies de l'Industrie et du Développement Durable</h6>
+          <p>
+          Acquisition des bases de l'informatique et sensibilisation au développement durable. 
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "#F07B3F", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  #F07B3F" }}
+          date= "2016 à Anizy-Le-Château"
+          iconStyle={{ background: "#F07B3F", color: "#fff" }}
+          icon={<GiSchoolBag/>}
+        >
+          <h3 className="vertical-timeline-element-title">Brevet des collèges</h3>
+          <h6 className="vertical-timeline-element-subtitle">On a vraiment besoin d'aller si loin ?</h6>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </div>
   );
 }
