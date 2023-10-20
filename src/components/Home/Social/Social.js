484c93b147b46a78006d8aeb311729300cc397b5
Maxime-Guillemin <max.guillemin@free.fr>
2023-10-20 15:11:01 +0200

    animation acceuil

--- src/components/Home/Social/Social.js
+++ src/components/Home/Social/Social.js
@@ -1,56 +1,97 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 import { SocialIcon } from 'react-social-icons';
 import { ThemeContext } from 'styled-components';
 
+import { motion } from "framer-motion"
+
+import "./Social.css"
+
 const styles = {
     iconStyle: {
-      marginLeft: 10,
-      marginRight: 10,
-      marginBottom: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 10,
         marginTop: 15,
     },
-  };
+};
 
 function Social() {
     const theme = useContext(ThemeContext);
 
-    const fgColor = theme.socialIconBgColor=== "#121212"? "#fefefe" : "#121212";
+    const container = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2
+            }
+        }
+    };
+
+    const item = {
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+            y: 0,
+            opacity: 1
+        }
+    };
+
+    const fgColor = theme.socialIconBgColor === "#121212" ? "#fefefe" : "#121212";
     return (
-        
+
         <div>
 
-            <SocialIcon
-                key="linkedin"
-                fgColor={fgColor}
-                 style={styles.iconStyle}
-                url="https://www.linkedin.com/in/maxime-guillemin-104370210/"
-                network="linkedin"
-                bgColor={theme.socialIconBgColor}
-                target="_blank"
-                rel="noopener"
-            />
+            <motion.ul
+                className="container"
+                variants={container}
+                initial="hidden"
+                animate="visible"
+            >
+                
+                <motion.li className="item" variants={item}>
 
-            <SocialIcon
-                key="github"
-                fgColor={fgColor}
-                 style={styles.iconStyle}
-                url="https://github.com/Maxime-Guillemin"
-                network="github"
-                bgColor={theme.socialIconBgColor}
-                target="_blank"
-                rel="noopener"
-            />
+                    <SocialIcon
+                        key="linkedin"
+                        fgColor={fgColor}
+                        style={styles.iconStyle}
+                        url="https://www.linkedin.com/in/maxime-guillemin-104370210/"
+                        network="linkedin"
+                        bgColor={theme.socialIconBgColor}
+                        target="_blank"
+                        rel="noopener"
+                    />
+                </motion.li>
 
-            <SocialIcon
-                key="email"
-                fgColor={fgColor}
-                 style={styles.iconStyle}
-                url="mailto:max.guillemin@free.fr"
-                network="email"
-                bgColor={theme.socialIconBgColor}
-                target="_blank"
-                rel="noopener"
-            />
+                <motion.li className="item" variants={item}>
+                   
+                    <SocialIcon
+                        key="github"
+                        fgColor={fgColor}
+                        style={styles.iconStyle}
+                        url="https://github.com/Maxime-Guillemin"
+                        network="github"
+                        bgColor={theme.socialIconBgColor}
+                        target="_blank"
+                        rel="noopener"
+                    />
+                </motion.li>
+
+                <motion.li className="item" variants={item}>
+                    
+                    <SocialIcon
+                        key="email"
+                        fgColor={fgColor}
+                        style={styles.iconStyle}
+                        url="mailto:max.guillemin@free.fr"
+                        network="email"
+                        bgColor={theme.socialIconBgColor}
+                        target="_blank"
+                        rel="noopener"
+                    />
+                </motion.li>
+            </motion.ul>
         </div>
     )
 
