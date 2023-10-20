a06b533c7268790c27988f0d57fafdae83900972
Maxime-Guillemin <max.guillemin@free.fr>
2023-10-20 16:23:13 +0200

    ajout projet

--- src/components/Projets/Projets.js
+++ src/components/Projets/Projets.js
@@ -18,6 +18,8 @@ import LaVraieMifa from '../../assets/LaVraieMifa.jpg';
 import LaravelOutil from '../../assets/LaravelOutils.jpg';
 import CentreVille from '../../assets/CentreVille.jpg';
 import Portfolio from '../../assets/Portfolio.jpg';
+import IndicateurOdema from '../../assets/IndicateurOdema.jpg';
+
 
 const Projets = () => {
 
@@ -65,8 +67,8 @@ const Projets = () => {
           <motion.div variants={item} className="card-container">
             <Card style={{ backgroundColor: "#e84545" } } title="La Vraie Mifa" bordered={false}>
             <img className="card-image mb-2" src={LaVraieMifa} alt="LaVraieMifa" />
-            <Tag color={color.symfony}>Symfony</Tag>
-            <Tag color={color.php}>Php</Tag>
+                <Tag color={color.php}>Php</Tag>
+                <Tag color={color.symfony}>Symfony</Tag>
             <p className='mt-2'>Site web réalisé entre amis pour découvrir Symfony et la configuration serveur.</p>
               <div className="icon-container">
                 <a href="https://github.com/votre-utilisateur/LaVraieMifa" target="_blank" rel="noopener noreferrer">
@@ -81,10 +83,10 @@ const Projets = () => {
       </Col>
       <Col lg="3">
         <motion.div variants={item} className="card-container">
-            <Card style={{ backgroundColor: "#2d4059" }} title="Laravel, outils" className='bg-red' bordered={false}>
-              <img className="card-image mb-2" src={LaravelOutil} alt="LaVraieMifa" />
-              <Tag color={color.laravel}>Laravel</Tag>
-              <Tag color={color.php}>Php</Tag>
+            <Card style={{ backgroundColor: "#2d4059" }} title="Laravel, outils" bordered={false}>
+              <img className="card-image mb-2" src={LaravelOutil} alt="Laraveloutil" />
+                <Tag color={color.php}>Php</Tag>
+                <Tag color={color.laravel}>Laravel</Tag>
             <p className='mt-2'>Projet pédagogique, découverte de laravel.</p>
               <a href="https://github.com/VoltalitSama/Laravel" target="_blank" rel="noopener noreferrer">
                 <li>
@@ -97,7 +99,7 @@ const Projets = () => {
       <Col lg="3">
         <motion.div variants={item} className="card-container">
             <Card style={{ backgroundColor: "#0C7C59" }} title="Tableau de bord centre-ville" className='bg-cyan' bordered={false}>
-              <img className="card-image mb-2" src={CentreVille} alt="LaVraieMifa" />
+              <img className="card-image mb-2" src={CentreVille} alt="CentreVille" />
               <Tag color={color.javascript}>JavaScript</Tag>
             <p className='mt-2'>Tableau de bord dans le cadre de mon travail à la région HDF.</p>
               <a href="https://github.com/MaximeGuillemin/dashboard-cv" target="_blank" rel="noopener noreferrer">
@@ -116,17 +118,17 @@ const Projets = () => {
       <Row className="justify-content-md-center mt-5">
         <Col lg="3">
           <motion.div variants={item} className="card-container">
-            <Card style={{ backgroundColor: "#2d4059" }} title="Portfolio" className='bg-red' bordered={false}>
-              <img className="card-image mb-2" src={Portfolio} alt="LaVraieMifa" />
+            <Card style={{ backgroundColor: "#2d4059" }} title="Portfolio" bordered={false}>
+              <img className="card-image mb-2" src={Portfolio} alt="Portfolio" />
               <Tag color={color.laravel}>JavaScript</Tag>
               <Tag color={color.react}>React</Tag>
               <p className='mt-2'>Mon portfolio :D</p>
-              <a href="https://github.com/votre-utilisateur/LaVraieMifa" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/Maxime-Guillemin/portfolio" target="_blank" rel="noopener noreferrer">
                 <li>
                   <i className="card-icon git"> <AiFillGithub size={50} title="Git" /></i>
                 </li>
               </a>
-              <a href="https://lavraiemifa.fr/" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.maxime-guillemin.fr/" target="_blank" rel="noopener noreferrer">
                 <i className="card-icon link"> <AiOutlineLink size={50} title="Git" /></i>
               </a>
             </Card>
@@ -134,17 +136,17 @@ const Projets = () => {
         </Col>
         <Col lg="3">
           <motion.div variants={item} className="card-container">
-            <Card style={{ backgroundColor: "#e84545" }} title="Projet indicateur ODEMA" className='bg-red' bordered={false}>
-              <img className="card-image mb-2" src={LaVraieMifa} alt="LaVraieMifa" />
+              <Card style={{ backgroundColor: "#0C7C59" }} title="Projet indicateur ODEMA" bordered={false}>
+              <img className="card-image mb-2" src={IndicateurOdema} alt="IndicateurOdema" />
               <Tag color={color.typescrypt}>TypeScript</Tag>
               <Tag color={color.react}>React</Tag>
-              <p className='mt-2'>Site web réalisé entre amis pour découvrir Symfony et la configuration serveur.</p>
-              <a href="https://github.com/votre-utilisateur/LaVraieMifa" target="_blank" rel="noopener noreferrer">
+              <p className='mt-2'>Tableau de bord pour l'Observatoire déchets et matière dans le cadre de mon travail à la région HDF.</p>
+                <a href="https://github.com/geo2france/odema-viz" target="_blank" rel="noopener noreferrer">
                 <li>
                   <i className="card-icon git"> <AiFillGithub size={50} title="Git" /></i>
                 </li>
               </a>
-              <a href="https://lavraiemifa.fr/" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.geo2france.fr/public/odema/indicateur/" target="_blank" rel="noopener noreferrer">
                 <i className="card-icon link"> <AiOutlineLink size={50} title="Git" /></i>
               </a>
             </Card>
